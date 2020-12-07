0b3f80ae63f29ba22e73580cb0b56792ff701b06
Heltzel <heltzel.service@hotmail.com>
2020-12-07 12:27:35 +0100

    Setup components and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import uuid from 'uuid'
 import './App.css'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <TodoInput />
+      <TodoList />
     </div>
   )
 }
